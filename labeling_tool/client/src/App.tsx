a9f368fb3f0c21587ad32d1972058e03ac2f151f
thienanh1999 <thienanh313@gmail.com>
2021-10-29 19:04:04 +0700

    [Labeling tool] Draw UI

--- labeling_tool/client/src/App.tsx
+++ labeling_tool/client/src/App.tsx
@@ -5,12 +5,14 @@ import {
 import Main from './components';
 import Register from "./components/register/Register";
 import history from './history';
+import Dashborad from "./components/dashboard/Dashboard";
 
 function App() {
 	return (
 		<Router history={history}>
 			<Route exact path='/' component={Main} />
 			<Route path='/register' component={Register} />
+			<Route path='/dashboard' component={Dashborad} />
 		</Router>
 	);
 }
