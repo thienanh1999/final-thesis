015b04f18edc7383d68ec153eca2cc6773493c0e
thienanh1999 <thienanh313@gmail.com>
2021-11-16 22:59:01 +0700

    update mysql database design

--- labeling_tool/server/app/api/models.py
+++ labeling_tool/server/app/api/models.py
@@ -31,6 +31,8 @@ class Project(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.CharField(max_length=1000)
     owner = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    k = models.FloatField()
+    b1 = models.FloatField()
 
     def to_dict(self):
         return {
@@ -53,13 +55,42 @@ class ProjectMember(models.Model):
 
 
 class Document(models.Model):
-    es_id = models.CharField(max_length=255)
+    es_id = models.IntegerField()
     project = models.ForeignKey(Project, on_delete=models.DO_NOTHING)
-    is_highlighted = models.BooleanField(default=False)
-    orders = models.TextField()
+    is_processed = models.BooleanField(default=False)
 
     class Meta:
         db_table = "document"
+
+
+class Sentence(models.Model):
+    id_in_document = models.IntegerField()
+    document = models.ForeignKey(Document, on_delete=models.DO_NOTHING)
+    context = models.TextField()
+    is_highlighted = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = "sentence"
+
+
+class TableData(models.Model):
+    id_in_document = models.IntegerField()
+    document = models.ForeignKey(Document, on_delete=models.DO_NOTHING)
+    is_highlighted = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = "tableData"
+
+
+class Cell(models.Model):
+    row = models.IntegerField()
+    col = models.IntegerField()
+    is_header = models.BooleanField()
+    table_data = models.ForeignKey(TableData, on_delete=models.DO_NOTHING)
+    context = models.TextField()
+
+    class Meta:
+        db_table = "cell"
 
 
 class Claim(models.Model):
@@ -70,6 +101,8 @@ class Claim(models.Model):
     content = models.TextField()
     is_labeled = models.BooleanField(default=False)
     label = models.CharField(max_length=20, default='')
+    created_by = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    annotated_by = models.ForeignKey(User, on_delete=models.DO_NOTHING)
 
     class Meta:
         db_table = "claim"
@@ -77,8 +110,8 @@ class Claim(models.Model):
 
 class Evidence(models.Model):
     claim = models.ForeignKey(Claim, on_delete=models.DO_NOTHING)
-    content = models.TextField()
-    context = models.TextField()
+    sentence = models.ForeignKey(Sentence, on_delete=models.DO_NOTHING, default=None)
+    cell = models.ForeignKey(Cell, on_delete=models.DO_NOTHING, default=None)
 
     class Meta:
         db_table = "evidence"
@@ -86,7 +119,10 @@ class Evidence(models.Model):
 
 class Annotator(models.Model):
     claim = models.ForeignKey(Claim, on_delete=models.DO_NOTHING)
-    annotators = models.TextField()
+    operation = models.TextField(max_length=50)
+    document = models.ForeignKey(Document, on_delete=models.DO_NOTHING, default=None)
+    sentence = models.ForeignKey(Sentence, on_delete=models.DO_NOTHING, default=None)
+    cell = models.ForeignKey(Sentence, on_delete=models.DO_NOTHING, default=None)
 
     class Meta:
         db_table = "annotator"
